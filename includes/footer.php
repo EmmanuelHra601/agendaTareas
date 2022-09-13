1e17461cb6cd92d874d97647353e4ceeec49ec1a
EmmanuelHra601 <guerreroherrerafcoemmanuel@gmail.com>
2022-09-12 21:46:12 -0500

    nueva actulizacion 2

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
-<script src="/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="libs/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
